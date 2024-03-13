51de01f5ba5714cdd49fd7580986b2270abd95b7
friedooo <friedlich3@gmail.com>
2024-03-13 20:12:25 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    <script src="https://api-maps.yandex.ru/v3/?apikey=c265821f-6725-461a-a56c-84c43b415a60&lang=ru_RU"></script>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
